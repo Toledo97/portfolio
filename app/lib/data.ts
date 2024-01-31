50a93513b9b264c5b4a2a8a321b9620b6e19ac2f
Toledo97 <toledojr7@hotmail.com>
2024-01-30 20:56:06 -0600

    adding some data changes, tutorial parts still included

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,21 +9,18 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
+
 
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  noStore();
 
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -33,6 +30,8 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  noStore();
+
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -53,6 +52,8 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore();
+
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -92,6 +93,8 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+  noStore();
+
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -124,6 +127,8 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  noStore();
+
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -145,6 +150,8 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore();
+
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -161,7 +168,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
@@ -188,6 +195,8 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  noStore();
+
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
@@ -229,3 +238,144 @@ export async function getUser(email: string) {
     throw new Error('Failed to fetch user.');
   }
 }
+
+///////////////////////////////////////////////////////
+
+const certificatesBundle = [
+  {
+    title: "IBM",
+    collection: [
+      {alt: "Web Development", src: "web-dev.png", w:175, h:0},
+      {alt: "IBM Accelerate", src: "IBMAccelerate.png", w:175, h:0},
+      {alt: "Professional Skills", src: "wps.png", w:175, h:0},
+    ]
+  },
+  {
+    title: "Coursera",
+    collection: [
+      {alt: "IBM Databases w/ Python", src: "DSQL.png", w:350, h:0},
+      {alt: "Databases w/ Python", src: "DBP.png", w:350, h:0},
+      {alt: "Python Data Structures", src: "PD.png", w:350, h:0},
+      {alt: "Programming for Everyone", src: "4Every1.png", w:350, h:0},
+    ]
+  },
+  {
+    title: "Nvidia",
+    collection: [
+      {alt: "Fundamentals of CUDA", src: "FundamentalsCUDA.png", w:350, h:0},
+      {alt: "Streams in CUDA", src: "acceleratedCUDA.png", w:350, h:0},
+      {alt: "Scaling Workloads w/ CUDA", src: "scalingCUDA.png", w:350, h:0},
+    ]
+  },
+]
+
+const skillsBundle = [
+  {
+    title: 'Languages',
+    collection: [
+      {alt: "Python", src: "python.png", w:50, h:0},
+      {alt: "JS", src: "js2.png", w:50, h:0},
+      {alt: "TS", src: "ts.png", w:50, h:0},
+      {alt: "C/C++", src: "cplus.png", w:45, h:0},
+    ]
+  },
+  {
+    title: 'Database Experience',
+    collection: [
+      {alt: "MySQL", src: "mysql.png", w:50, h:0},
+      {alt: "Mongo", src: "mongodb.png", w:50, h:0},
+      {alt: "Neo4j", src: "neo4j.png", w:50, h:40},
+    ]
+  },
+  {
+    title: 'Frontend Experience',
+    collection: [
+      {alt: "Angular", src: "angular.png", w:45, h:0},
+      {alt: "React", src: "react.png", w:50, h:0},
+      {alt: "MUI", src: "mui.png", w:50, h:0},
+      {alt: "Bootstrap", src: "boot.png", w:45, h:0},
+    ]
+  },
+  {
+    title: 'Technologies',
+    collection: [
+      {alt: "Git", src: "git.png", w:45, h:0},
+      {alt: "Node.js", src: "node.png", w:45, h:0},
+      {alt: "Next.js", src: "next.png", w:45, h:0},
+    ]
+  }
+]
+
+
+
+const companies = [
+{   imageData: {alt: "Carle Health", src: "carle.png", w:135, h:0}, 
+  location: 'Champaign, IL',
+  roles: [
+  {
+      title:"Sys-Ops & Care Mgmt Analyst", 
+      timeline: "Feb 2024 - Current",
+      description: []
+  }
+  ]
+},
+{   imageData: {alt: "Motorola Solutions", src: "Motorola-Solutions.png", w:275, h:0},
+  location: 'Champaign, IL',
+  roles: [
+  {
+      title:"Software Engineer: Frontend", 
+      timeline: "Sept 2023 - Dec 2023",
+      description: [
+      `Gained hands-on experience in writing unit tests and end-to-end tests 
+      for Angular applications, ensuring code reliability and quality.`,
+      `Wrote production ready code, conducted rigorous testing, review, and 
+      followed best practices for secure and reliable web app.`,
+      `Engaged in effective communication to convey ideas, suggestions, and 
+      project updates, ensuring myself and team members were well-informed and 
+      aligned with project goals.
+      `
+      ]
+  },
+  {
+      title:"Software Engineer: Backend", 
+      timeline: "May 2021 - May 2022",
+      description: [
+      `Used python to create a dynamic method of generating and replacing tailored 
+      MySQL procedures to optimize database performance based on a user\'s product ownership.`,
+      `Consolidated and cleaned data from CouchDB and Cosmos with Python, using 
+      multiprocessing libraries for seamless information flow across our application\'s pipeline.`,
+      `Engaged with an international teams in daily design meetings to drive 
+      project success and maintain clear communication to meet biweekly sprint goals.`
+      ]
+  }
+  ],
+},
+{   imageData: {alt: "IBM", src: "ibm.png", w:100, h:0},
+  location: 'Bloomginton, IL',
+  roles: [
+  {
+      title:"Software Developer", 
+      timeline: "May 2022 - Aug 2022",
+      description: [
+      `Proficient in leveraging RESTful APIs within Python development, utilizing tools like GitHub 
+      and ServiceNow to streamline collaboration and project management.`,
+      `Skilled in designing and implementing MySQL databases, seamlessly integrating 
+      them with Python applications to optimize data storage and retrieval capabilities.`,
+      `Engaged in secure coding practices incorporating user / pw protocols to safeguard 
+      sensitive user information and maintain system integrity.`
+      ]
+  },
+  ],
+},
+]
+
+
+
+
+
+
+
+
+
+
+export { companies, certificatesBundle, skillsBundle};
